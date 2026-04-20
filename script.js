74bd974faa05f09df665376b2046211acd896649
medev-fsai <256591220+medev-fsai@users.noreply.github.com>
2026-04-20 15:37:55 +0100

    Add basic functions for known operations: + - * /.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Welcome to try our calculator");
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+console.log(`Result of addition of ${2} and ${3} is ${add(2,3)}`);
+console.log(`Result of subtraction of ${2} from ${3} is ${subtract(2,3)}`);
+console.log(`Result of multiplication of ${2} and ${3} is ${multiply(2,3)}`);
+console.log(`Result of division of ${2} by ${3} is ${divide(2,3)}`);
