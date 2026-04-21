9ba98665d827f1d479f86fe6c4d43e5aa8e38bd5
medev-fsai <256591220+medev-fsai@users.noreply.github.com>
2026-04-21 11:28:32 +0100

    Add logic to perform one operation. Logic of updating displays, history and operation.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let operandOne = null;
 let operandTwo = null;
 let operator = null;
+let result = null;
 
 function add(num1, num2){
     return num1 + num2;
@@ -55,18 +56,111 @@ let equalityButton = document.querySelector('#equal');
 let pointButton = document.querySelector('#point');
 
 digitButtons.forEach(item => item.addEventListener("click", function(){
-    let operandOneValue = parseInt(item.innerText);
-    updateOperandOne(operandOneValue);
-    updateOperationDisplay();
+    if(checkOperatorIsNull()){
+        let operandOneValue = item.innerText;
+        const display = updateOperandOne(operandOneValue);
+        updateLowerDisplay(display);
+    }else if(checkResultIsNull()){
+        let operandTwoValue = item.innerText;
+        const display = updateOperandTwo(operandTwoValue);
+        updateLowerDisplay(display);
+    }else {
+        console.log("****");
+    }
 }));
 
 function updateOperandOne(value) {
-    operandOne = value;
+    operandOne = operandOne ?? 0; 
+    operandOne = parseInt(`${operandOne}` + value);
+    console.log("Operand One " + operandOne);
+    return operandOne;
+}
+
+function updateOperandTwo(value) {
+    operandTwo = operandTwo ?? 0;
+    operandTwo = parseInt(`${operandTwo}` + value);
+    console.log("Operand Two " + operandTwo);
+    return operandTwo;
+}
+
+function checkOperandOneIsNull(){
+    return operandOne === null;
+}
+function checkOperandTwoIsNull(){
+    return operandTwo === null;
+}
+function checkOperatorIsNull(){
+    return operator === null;
+}
+function checkResultIsNull(){
+    return result === null;
+}
+
+function updateOperator(value){
+    switch(value){
+        case "add":
+            operator = "+";
+            break;
+        case "subtract":
+            operator = "-";
+            break;
+        case "divide":
+            operator = "/";
+            break;
+        case "multiply":
+            operator = "*";
+            break;
+        default:
+            operator = "+";
+            break;
+    }
+}   
+
+function updateLowerDisplay(value){
+    const display = document.querySelector('.current-operation');
+    display.textContent = value;
     return;
 }
 
-function updateOperationDisplay(){
-    const display = document.querySelector('.current-operation');
-    display.textContent = operandOne;
+function clearLowerDisplay(){
+    document.querySelector('.current-operation').textContent = "";
     return;
 }
+
+function updateUpperDisplay(){
+    clearUpperDisplay();
+    const upperDisplay = document.querySelector(".last-operation");
+    let operandOneDisplayValue = operandOne;
+    let operandTwoDisplayValue = operandTwo;
+    let operatorDisplayValue = operator;
+    operandOneDisplayValue = operandOneDisplayValue ?? "";
+    operatorDisplayValue = operatorDisplayValue ?? "";
+    operandTwoDisplayValue = operandTwoDisplayValue ?? "";
+    upperDisplay.textContent += `${operandOneDisplayValue}${operatorDisplayValue}${operandTwoDisplayValue}`;
+    return; 
+}
+
+function clearUpperDisplay(){
+    document.querySelector('.last-operation').textContent = "";
+    return;
+}
+
+//console.log(operatorButtons);
+operatorButtons.forEach(item => item.addEventListener("click", function() {
+    if(!checkOperandOneIsNull()){
+        let operation = item.getAttribute('id');
+        updateOperator(operation);
+        clearLowerDisplay();
+        updateUpperDisplay();
+    }
+}));
+
+equalityButton.addEventListener("click", function() {
+    let proceed = (!checkOperandOneIsNull()) && (!checkOperatorIsNull()) && (!checkOperandTwoIsNull());
+    if(proceed){
+        //Operation is ready to be made
+        result = operate(operandOne, operator, operandTwo);
+        updateLowerDisplay(result);
+        updateUpperDisplay();
+    }
+});
