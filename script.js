48d7c6bb0f2144ea22c2307e41e8cd832c20152c
medev-fsai <256591220+medev-fsai@users.noreply.github.com>
2026-04-23 19:37:08 +0100

    Add logic for entering first num and operator.

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function processUserInput(event){
             updatePrev(input.type, input.value);
             //update display
             updateDisplay(operandOne);
-            return;
+            //return;
         }else if(input.type === "point"){
             //This case means user wants to enter a float
             operandOne = operandOne || "0";
@@ -53,26 +53,119 @@ function processUserInput(event){
                 updateDisplay(operandOne);
             }else{
                 //Operand One contains already a float.
-                return;
+                //return;
             }
         }else if(input.type === "equal"){
             //We only have one display which already contains the typed number.
             //We should exit. Operand One is still on its value.
-            return;
+            //return;
         }else if(input.type === "operator"){
+            //If user clicks operator before typing any number
+            //should default operand one to 0
+            operandOne = operandOne || "0";
+            //If operand One finished by "." then make it "digit.0"
+            operandOne = (operandOne.at(-1) === ".") ? operandOne.slice(0, -1) : operandOne;
             //Now users finished typing first number
-
+            state = "enteringOperator";
+            operator = input.value;
+            //Store use choice in prev
+            updatePrev(input.type, input.value);
+            //update display
+            updateDisplay(`${operandOne}`.concat('', operator));
+            //return;
         }else {
             //User clicked "delete" to correct previous click.
-            //case User clicked "clear" is already handled outside.
+            //case User clicked "clear" is already handled outside code block.
+            operandOne = operandOne || "0";
+            //Remove last digit from number.
+            operandOne = operandOne.slice(0, -1);
+            //Imagine user clicked . then delete. operand One would be "0"
+            //making a 0 display for the user which is not nice.
+            //force it to be ""
+            operandOne = (operandOne === "0") ? "" : operandOne;
+            //Store use choice in prev
+            updatePrev(input.type, input.value);
+            //update display
+            updateDisplay(operandOne);
+            //return;
         }
     }else if(state === "enteringOperator"){
+        if(input.type === "digit"){
+            //User already entered operator and Operand One  
+            //update Operand Two
+            operandTwo = `${operandTwo}`.concat('', input.value);
+            //update state
+            state = "enteringSecondOperand";  
+            //Store use choice in prev
+            updatePrev(input.type, input.value);
+            //update display
+            updateDisplay(operandTwo);
+            //return;
+    
+        }else if(input.type === "point"){
+            //User filled operand One, operator, and now clicks "."
+            //Interpret this as if he wants a second float operand
+            //This is consistent with previous behavior for operand One.
+            operandTwo = "0.";
+            state = "enteringSecondOperand";
+            //store user choice
+            updatePrev(input.type, input.value);
+            //update display
+            updateDisplay(`${operandOne}`.concat(operator, operandTwo));
+            //return;
+        }else if(input.type === "equal"){
+            //User entered operand One and an operator
+            //Then clicked "="
+            //return;
+        }else if(input.type === "operator"){
+            //User already entered an operator, and is filling another
+            //Interpret this as if he is changing operator.
+            operator = input.value;
+            //Store user choice
+            updatePrev(input.type, input.value);
+            //update display
+            updateDisplay(`${operandOne}`.concat('', operator));
+            //return;
+        }else{
+            //User clicked "delete" to remove his last choice
+            //Perhaps user wants to change just operator displayed (already guaranteed by clicking another operator)
+            //Or wants to change operand One (MOST LIKELY)
+            operator = null;
+            state = "enteringFirstOperand";
+            //Store user choice
+            updatePrev(input.type, input.value);
+            //update display
+            updateDisplay(`${operandOne}`);
+            //return;
 
+        }
     }else if(state === "enteringSecondOperand"){
+        if(input.type === "digit"){
 
+        }else if(input.type === "point"){
+
+        }else if(input.type === "equal"){
+
+        }else if(input.type === "operator"){
+            
+        }else{
+            
+        }
     }else{
+        if(input.type === "digit"){
 
+        }else if(input.type === "point"){
+
+        }else if(input.type === "equal"){
+
+        }else if(input.type === "operator"){
+            
+        }else{
+            
+        }
     }
+
+    console.log(operandOne + " : " + operator + " : " + operandTwo);
 };
 
 function initMemory(){
